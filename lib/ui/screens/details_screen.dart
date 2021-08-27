68a96f6d849063bbfffb67bc7a6d2954cd9bc724
nullskill <nullskill@github.com>
2021-08-27 18:30:52 +0300

    Add sliver_fab package

---
+++ lib/ui/screens/details_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/model/movie.dart';
+import 'package:movie_app/ui/res/colors.dart';
+
+class MovieDetailsScreen extends StatefulWidget {
+  final Movie movie;
+
+  const MovieDetailsScreen({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  _MovieDetailsScreenState createState() => _MovieDetailsScreenState();
+}
+
+class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.mainColor,
+      body: SizedBox(),
+    );
+  }
+}
